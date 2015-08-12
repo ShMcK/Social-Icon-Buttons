7cab4790105c05d310434871a98abb81c05a76f9
ShMcK <me@shmck.com>
2015-08-12 22:35:11 +0900

    counts added

---
+++ vanilla-js-social-buttons-counts/README.md
@@ -0,0 +1,7 @@
+# Social Buttons in Vanilla JS with Counters
+
+Run a server, for example [http-server](https://www.npmjs.com/package/http-server).
+
+    http-server
+    
+Open `index.html`
---
+++ vanilla-js-social-buttons-counts/social-buttons.js
@@ -0,0 +1,57 @@
+var url = encodeURI(document.URL);
+var text = encodeURI(document.title);
+
+function callFacebookAPI() {
+  window.open('//www.facebook.com/sharer/sharer.php?u=' + url + '&t=' + text, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+}
+
+function callTwitterAPI() {
+  window.open('//twitter.com/share?text=' + text, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=255, width=550');
+}
+
+function getJSON(url, success) {
+
+  var ud = '_' + +new Date,
+    script = document.createElement('script'),
+    head = document.getElementsByTagName('head')[0]
+      || document.documentElement;
+
+  window[ud] = function (data) {
+    head.removeChild(script);
+    success && success(data);
+  };
+
+  script.src = url.replace('callback=?', 'callback=' + ud);
+  head.appendChild(script);
+}
+
+/* get twitter count */
+getJSON("http://cdn.api.twitter.com/1/urls/count.json?url=" + url + "&callback=?", function (json) {
+  console.log('twitter', json.count);
+  if (json.count) {
+    return incrementCount(json.count, 'twCount');
+  }
+});
+
+/* get facebook shares */
+getJSON("http://graph.facebook.com/?id=http://" + url + "&callback=?", function (json) {
+  console.log('facebook', json.shares);
+  if (json.shares) {
+    return incrementCount(json.shares, 'fbCount');
+  }
+});
+
+function incrementCount(endValue, name) {
+  var times = 0;
+  var incrementer = {};
+  incrementer[name] = setInterval(function () {
+    // set new current
+    var newValue = Math.ceil(endValue / times);
+    // change DOM value
+    document.getElementsByClassName("fbCount")[0].innerHTML = newValue;
+    // exit on 10th increment
+    if (++times === 10) {
+      window.clearInterval(incrementer[name]);
+    }
+  }, 100);
+}
